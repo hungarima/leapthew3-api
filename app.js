b25f991a20a505ff614cf19ed803bdcc181eb77d
hungarima <tronghung199@gmail.comm>
2018-07-06 00:35:34 +0700

    Modifided to fit

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const config = require("./config-production.json");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const userRouter = require("./modules/api/users/router");
+const authRouter = require("./modules/api/auth/router");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+
+
+app.use(express.static('./public'));
+
+app.get('/', function(req, res) {
+  res.send('Leap The W3');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+mongoose.connect(config.mongoPath, err => {
+  if (err) console.error(err);
+  else console.log("Database connect successful");
 });
 
-module.exports = app;
+const port = process.env.PORT || 6969;
+
+app.listen(port, err => {
+  if (err) console.log(err);
+  console.log("Server started at port " + port);
+});
